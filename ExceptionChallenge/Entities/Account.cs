c8dca745a70c39440dae28aef8f46c2c605a91ba
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-24 19:52:31 -0300

    first commit

---
+++ ExceptionChallenge/Entities/Account.cs
@@ -0,0 +1,57 @@
+﻿using ExceptionChallenge.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionChallenge.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            if(balance < 0)
+            {
+                throw new DomainException("The Initial Balance must be positive.");
+            }
+            if(withdrawLimit < 0)
+            {
+                throw new DomainException("The Withdraw Limit must be positive.");
+            }
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount > 0)
+            {
+                Balance += amount;
+            }
+            else
+            {
+                throw new DomainException("ERROR! This value must be positive.");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainException("ERROR! The value to Withdraw must be below of the Withdraw Limit : " + WithdrawLimit.ToString("F2",CultureInfo.InvariantCulture));
+            }else if(amount > Balance)
+            {
+                throw new DomainException("ERROR! Insufficient Funds.");
+            }
+        }
+
+    }
+}
---
+++ ExceptionChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder : ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance : ");
+            double initialBalance = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
